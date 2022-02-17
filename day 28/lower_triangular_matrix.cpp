80336ea6d00423996790af9706c2a4c27f0db9d6
Ratnakar <ratnakar3895@yahoo.com>
2022-02-17 19:39:13 +0530

    Day 28 : Lower Triangular Matrix - Column Major

--- day 28/lower_triangular_matrix.cpp
+++ day 28/lower_triangular_matrix.cpp
@@ -42,24 +42,65 @@ void Lower_Triangular_RM::display()
     }
 }
 
+class Lower_Triangular_CM
+{
+    int *A;
+    int n;
+
+public:
+    Lower_Triangular_CM(int n)
+    {
+        A = new int[n * (n + 1) / 2];
+        this->n = n;
+    }
+    void set(int, int, int);
+    int get(int, int);
+    void display();
+    ~Lower_Triangular_CM() { delete[] A; }
+};
+
+void Lower_Triangular_CM::set(int i, int j, int x)
+{
+    if (i >= j)
+        A[((n * (j - 2)) - ((j - 1) * (j - 2) / 2)) + (i - j)] = x;
+}
+
+int Lower_Triangular_CM::get(int i, int j)
+{
+    if (i >= j)
+        return A[((n * (j - 2)) - ((j - 1) * (j - 2) / 2)) + (i - j)];
+    return 0;
+}
+
+void Lower_Triangular_CM::display()
+{
+    for (int i = 1; i <= this->n; i++)
+    {
+        for (int j = 1; j <= this->n; j++)
+            cout << get(i, j) << " ";
+        cout << endl;
+    }
+}
+
 int main()
 {
-    Lower_Triangular_RM l1(5);
+    // Lower_Triangular_RM l1(5);
+    Lower_Triangular_CM l1(5);
     l1.set(1, 1, 5);
-    l1.set(2, 1, 10);
-    l1.set(2, 2, 15);
-    l1.set(3, 1, 20);
-    l1.set(3, 2, 25);
-    l1.set(3, 3, 30);
-    l1.set(4, 1, 35);
-    l1.set(4, 2, 40);
-    l1.set(4, 3, 45);
-    l1.set(4, 4, 50);
-    l1.set(5, 1, 55);
-    l1.set(5, 2, 60);
-    l1.set(5, 3, 65);
-    l1.set(5, 4, 70);
-    l1.set(5, 5, 75);
+    l1.set(2, 1, 5);
+    l1.set(2, 2, 5);
+    l1.set(3, 1, 5);
+    l1.set(3, 2, 5);
+    l1.set(3, 3, 5);
+    l1.set(4, 1, 5);
+    l1.set(4, 2, 5);
+    l1.set(4, 3, 5);
+    l1.set(4, 4, 5);
+    l1.set(5, 1, 5);
+    l1.set(5, 2, 5);
+    l1.set(5, 3, 5);
+    l1.set(5, 4, 5);
+    l1.set(5, 5, 5);
 
     l1.display();
 
