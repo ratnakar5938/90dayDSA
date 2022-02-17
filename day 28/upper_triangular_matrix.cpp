e3d99d0a33c0625e294857b1159da2c6a8034737
Ratnakar <ratnakar3895@yahoo.com>
2022-02-17 21:16:26 +0530

    Day 28 : Upper Triangular Matrix - Column Major

--- day 28/upper_triangular_matrix.cpp
+++ day 28/upper_triangular_matrix.cpp
@@ -42,23 +42,52 @@ void Upper_Triangular_RM::display()
     }
 }
 
+class Upper_Triangular_CM
+{
+    int *A;
+    int n;
+
+public:
+    Upper_Triangular_CM(int n)
+    {
+        A = new int[n * (n - 1) / 2];
+        this->n = n;
+    }
+    void set(int, int, int);
+    int get(int, int);
+    void display();
+    ~Upper_Triangular_CM() { delete[] A; }
+};
+
+void Upper_Triangular_CM::set(int i, int j, int x)
+{
+    if (i <= j)
+        A[(j * (j - 1) / 2) + (j - i)] = x;
+}
+
+int Upper_Triangular_CM::get(int i, int j)
+{
+    if (i <= j)
+        return A[(j * (j - 1) / 2) + (j - i)];
+    return 0;
+}
+
+void Upper_Triangular_CM::display()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cout << get(i, j) << " ";
+        cout << endl;
+    }
+}
+
 int main()
 {
-    // int n;
-    // cout << "Enter Dimension : ";
-    // cin >> n;
-    Upper_Triangular_RM u1(5);
 
-    // int x;
-    // cout << "Enter the elements : \n";
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         cin >> x;
-    //         u1.set(i, j, x);
-    //     }
-    // }
+    // Upper_Triangular_RM u1(5);
+    Upper_Triangular_CM u1(5);
+
     u1.set(1, 1, 5);
     u1.set(1, 2, 5);
     u1.set(1, 3, 5);
